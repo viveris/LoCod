11e421cd9ca10990712c52198cccde535494a864
Hugo KIEFFER <hugo.kieffer@viveris.fr>
2021-02-23 16:40:54 +0100

    [CI] Add CI

---
+++ demo/simple-int-mul/main.c
@@ -0,0 +1,40 @@
+#ifndef LOCOD_FPGA
+#include "locod.h"
+#endif
+
+void addition(int *a, int *b, int *result)
+{
+	*result = *a + *b;
+}
+
+void multiplication(int *a, int *b, int *result)
+{
+	*result = *a * *b;
+}
+
+#ifndef LOCOD_FPGA
+int main(int argc, char **argv)
+{
+	int a = 16;
+	int b = 4;
+	int result = 0;
+
+	struct param param_a = { 0 };
+	param_a.p = &a;
+	param_a.len = sizeof(int);
+
+	struct param param_b = { 0 };
+	param_b.p = &b;
+	param_b.len = sizeof(int);
+
+	struct param param_result = { 0 };
+	param_result.p = &result;
+	param_result.len = sizeof(int);
+
+	CPU(addition, &a, &b, &result);
+	fprintf(stdout, "Add result = %d\n", result);
+
+	FPGA(multiplication, param_a, param_b, param_result);
+	fprintf(stdout, "Mul result = %d\n", result);
+}
+#endif
