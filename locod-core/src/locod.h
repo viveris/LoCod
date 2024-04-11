b79f69f258667dbbfc49b9255610bc18e3789b2b
Julien Armengaud <julien.armengaud@viveris.fr>
2024-04-11 14:22:45 +0200

    [UPDATE] Add headers to source files

--- locod-core/src/locod.h
+++ locod-core/src/locod.h
@@ -1,3 +1,35 @@
+/*                      __            ___             _ 
+ *                     / /    ___    / __\  ___    __| |
+ *                    / /    / _ \  / /    / _ \  / _` |
+ *                   / /___ | (_) |/ /___ | (_) || (_| |
+ *                   \____/  \___/ \____/  \___/  \__,_|
+ *
+ *             ***********************************************
+ *                              LoCod Project
+ *                  URL: https://github.com/viveris/LoCod
+ *             ***********************************************
+ *                  Copyright © 2024 Viveris Technologies
+ *
+ *                   Developed in partnership with CNES
+ *               (DTN/TVO/ET: On-Board Data Handling Office)
+ *
+ *   This file is part of the LoCod framework.
+ *
+ *   The LoCod framework is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 #ifndef LOCOD_H
 #define LOCOD_H
 
